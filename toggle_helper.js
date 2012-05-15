d32ee7f694a20d2ea99976f21dcfef657c1e1192
Cole van Krieken <colevankrieken@gmail.com>
2012-05-14 23:16:21 -0400

    Moved hide.css inside toggle_helper.js

--- toggle_helper.js
+++ toggle_helper.js
@@ -1,8 +1,10 @@
-var hide_images = $(document.createElement("style")).attr("src", "hide.css")
+var hide_images = $(document.createElement("style"))
+	.attr("id", "cathider")
+	.attr("type", "text/css")
+	.text("img { visibility: hidden; } img.cat { visibility: visible !important; }");
 var color_kittens;
 var toggled;
 
-// color_kittens is defined in the options
 function getKittenURL(width, height) {
 	var modifier = color_kittens ? "" : "g/";
 	return "http://www.placekitten.com/" + modifier + width + "/" + height;
